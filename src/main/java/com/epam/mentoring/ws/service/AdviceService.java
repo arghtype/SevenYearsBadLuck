a7bff3e7b8b988d29b36e6c4f4c83ca2cee26ff5
myashin <Maksim.Yashin@homecredit.ru>
2014-04-19 13:02:41 +0400

    Refactoring in AdviceService according to code review remarks.

--- src/main/java/com/epam/mentoring/ws/service/AdviceService.java
+++ src/main/java/com/epam/mentoring/ws/service/AdviceService.java
@@ -6,65 +6,74 @@ import java.util.*;
  * @author Maksim_Yashin@epam.com
  */
 public class AdviceService {
-    private static final List<String> advices = new LinkedList<>();
-    private static final List<String> badAdvices = new LinkedList<>();
+    private static final List<String> neutral;
+    private static final List<String> badLuck;
     private static final String ULTIMATE_UNIVERSE_ADVICE = "42";
     private static final Date tooYoungDate;
+    private final Random rnd;
 
     static {
-        badAdvices.add("YOU WILL DIE ALONE");
-        badAdvices.add("YOU ARE THROWING YOUR LIFE AWAY");
-        advices.add("GO OUTSIDE!");
-        advices.add("ASK AGAIN LATER");
-        advices.add("THINK FOR YOURSELF");
-        advices.add("QUESTION AUTHORITY");
-        advices.add("DON'T LEAVE THE HOUSE TODAY");
-        badAdvices.add("GIVE UP!");
-        advices.add("MARRY AND REPRODUCE");
-        badAdvices.add("STAY ASLEEP");
-        advices.add("WAKE UP");
-        badAdvices.add("WE WILL ALL DIE ONE DAY");
-        advices.add("FOLLOW THE WHITE RABBIT");
-        advices.add("TODAY WAS A GOOD DAY");
-        advices.add("WELCOME HOME");
-        advices.add("STAY FOREVER YOUNG");
-        advices.add("DON'T CROSS THE STREET");
-        advices.add("DON'T STOP");
-        advices.add("DON'T LOOK BACK");
-        badAdvices.add("THEY KNOW");
-        advices.add("DON'T LOOK BACK");
-        advices.add("TOMORROW COMES TODAY");
-        advices.add("NEVER STOP TRYING");
-        advices.add("DIE FOR METAL");
-        badAdvices.add("THE CAKE IS A LIE");
-        badAdvices.add("TRUST NO ONE");
-        badAdvices.add("THEY ARE WATCHING");
-        advices.add("RUN");
-        badAdvices.add("YOU CAN'T ESCAPE");
-        advices.addAll(badAdvices);
+        badLuck = Arrays.asList(
+                "YOU WILL DIE ALONE",
+                "YOU ARE THROWING YOUR LIFE AWAY",
+                "GIVE UP!",
+                "STAY ASLEEP",
+                "WE WILL ALL DIE ONE DAY",
+                "THEY KNOW",
+                "THE CAKE IS A LIE",
+                "TRUST NO ONE",
+                "THEY ARE WATCHING",
+                "YOU CAN'T ESCAPE",
+                "NO ONE LOVES YOU",
+                "YOU ARE NOT A SMART MAN, AREN'T YOU?"
+        );
+        neutral = Arrays.asList(
+                "GO OUTSIDE!",
+                "ASK AGAIN LATER",
+                "THINK FOR YOURSELF",
+                "QUESTION AUTHORITY",
+                "DON'T LEAVE THE HOUSE TODAY",
+                "MARRY AND REPRODUCE",
+                "WAKE UP",
+                "FOLLOW THE WHITE RABBIT",
+                "TODAY WAS A GOOD DAY",
+                "WELCOME HOME",
+                "STAY FOREVER YOUNG",
+                "DON'T CROSS THE STREET",
+                "DON'T STOP",
+                "DON'T LOOK BACK",
+                "DON'T LOOK BACK",
+                "TOMORROW COMES TODAY",
+                "NEVER STOP TRYING",
+                "DIE FOR METAL",
+                "RUN"
+        );
 
         Calendar cal = new GregorianCalendar();
         cal.set(1996, Calendar.JANUARY, 1);
         tooYoungDate = cal.getTime();
     }
 
+    public AdviceService() {
+        rnd = new Random();
+    }
+
     public String getRandomAdvice() {
-        return getRandomAdviceFromList(advices);
+        return getRandomAdviceFromList(neutral);
     }
 
     public String getAdviceForPerson(Date birthDate, String name) {
-        if (tooYoungDate.before(birthDate)){
+        if (tooYoungDate.before(birthDate)) {
             return getAdviceToPerson(name, ULTIMATE_UNIVERSE_ADVICE);
         }
-
-        return  getAdviceToPerson(name, getRandomAdviceFromList(badAdvices));
+        return getAdviceToPerson(name, getRandomAdviceFromList(badLuck));
     }
 
-    private String getAdviceToPerson(String name, String advice){
+    private String getAdviceToPerson(String name, String advice) {
         return name + ", " + advice;
     }
-    private String getRandomAdviceFromList(List<String> adviceList){
-        Collections.shuffle(adviceList);
-        return adviceList.get(0);
+
+    private String getRandomAdviceFromList(List<String> adviceList) {
+        return adviceList.get(rnd.nextInt(adviceList.size()));
     }
 }
